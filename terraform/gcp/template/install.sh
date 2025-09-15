8d40b8126eb75e07ef1f0a38b7777aa312eb0a30
vinodbhorge <vinodbbhorge@gmail.com>
2025-09-15 11:55:59 +0530

    Updated update root org function. And repositioned the form_data.csv

--- terraform/gcp/template/install.sh
+++ terraform/gcp/template/install.sh
@@ -258,29 +258,44 @@ function get_new_root_org() {
         echo "Error: Could not fetch rootOrgId"
         return 1
     fi
-    echo "$root_org"
+
+    echo "$root_org|$host"
 }
 
 function update_root_org() {
     local environment="$1"
+    local new_root_org_and_host
+    new_root_org_and_host="$(get_new_root_org)" || return 1
+
     local new_root_org
-    new_root_org="$(get_new_root_org)" || return 1
-    local backup_dir="../../../cassandra-backup/$environment"
-    if [ -d "$backup_dir" ]; then
-        cd "$backup_dir" || return
-    else
-        echo "Backup directory $backup_dir not found!"
+    local new_host
+    new_root_org=$(echo "$new_root_org_and_host" | cut -d'|' -f1)
+    new_host=$(echo "$new_root_org_and_host" | cut -d'|' -f2)
+
+    local source_dir="../../../cassandra-backup"
+    local backup_dir="$source_dir/$environment"
+
+    if [ ! -d "$source_dir" ]; then
+        echo "Source directory $source_dir not found!"
         return 1
     fi
-    if [ ! -f form_data.csv ]; then
-        echo "form_data.csv not found in $backup_dir"
+
+    if [ ! -f "$source_dir/form_data.csv" ]; then
+        echo "form_data.csv not found in $source_dir"
         return 1
     fi
-    awk -F',' -v new_id="$new_root_org" 'BEGIN{OFS=","}
+
+    mkdir -p "$backup_dir"
+
+    awk -F',' -v new_id="$new_root_org" -v new_host="$new_host" 'BEGIN{OFS=","}
         NR==1 {print; next}
-        $1!="*" {$1=new_id}
+        {
+            if ($1!="*") $1=new_id
+            gsub("https://gcp-fmps.sunbirded.org", new_host, $0)
+        }
         {print}
-    ' form_data.csv > form_data_updated.csv
+    ' "$source_dir/form_data.csv" > "$backup_dir/form_data_updated.csv"
+
     echo "Updated file created at: $backup_dir/form_data_updated.csv"
 }
 
